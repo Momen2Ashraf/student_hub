ce6c946f7c1528965d99ad19699c193e462f5be8
momenmust <200013269@must.edu.eg>
2025-07-24 21:05:02 +0300

    Student class

--- student_hub/src/main/kotlin/Main.kt
+++ student_hub/src/main/kotlin/Main.kt
@@ -3,35 +3,19 @@ package org.example
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val student = Student(id = 1, "Momen",2023,"momen@gmail.com")
-    println("welcome admin would you like to ")
-
-    while (true){
-        println(
-        "1- Add student \n"+
-        "2- update student info \n"+
-        "3- remove student \n"+
-        "4- list students"
-    )
-       print("enter your choice : ")
-    val choice = readLine()
-    when(choice){
-        "1" -> addStudent()
-        "2" -> updateStudentInfo()
-        "3" -> removeStudent()
-        "4" -> getStudents()
-        else -> println("invalid input try again")
+    val student = Student(id = 1, "Momen", 2023, "momen@gmail.com")
+    val choice = Command.AddStudent(student)
+    when (choice) {
+        is Command.AddStudent -> { addStudent() }
+        is Command.UpdateStudentInfo ->{updateStudentInfo()}
+        is Command.RemoveStudent -> {removeStudent()}
+        is Command.GetAllStudents -> {getStudents()}
+        is Command.FilterStudents -> {}
+        is Command.GetStudentById -> {}
     }
-       print("would you like to make another operation (Y|N) : ")
-       val char = readLine()
-       if (char == "n" || char == "N")
-           break
-       else
-           continue
-   }
 }
 fun addStudent (){
-
+    println("Added successfully !!")
 }
 fun updateStudentInfo(){
 
