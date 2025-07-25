4b46da0195166dcdadb84b0035f059eeba47a242
MirvtMohamed <120274557+MirvtMohamed@users.noreply.github.com>
2025-07-25 06:20:26 +0300

    Remove Function

--- student_hub/src/main/kotlin/Main.kt
+++ student_hub/src/main/kotlin/Main.kt
@@ -3,12 +3,19 @@ package org.example
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
+
+    val studentList = mutableListOf(
+        Student(1, "Mirvt", 2022, "mirvt@gmail.com"),
+        Student(2, "Ahmed", 2023, "ahmed@gmail.com"),
+        Student(3, "Laila", 2021, "laila@gmail.com")
+    )
+
     val student = Student(id = 1, "Momen", 2023, "momen@gmail.com")
     val choice = Command.AddStudent(student)
     when (choice) {
         is Command.AddStudent -> { addStudent() }
         is Command.UpdateStudentInfo ->{updateStudentInfo()}
-        is Command.RemoveStudent -> {removeStudent()}
+        is Command.RemoveStudent -> removeStudent(choice.student.id)
         is Command.GetAllStudents -> {getStudents()}
         is Command.FilterStudents -> {}
         is Command.GetStudentById -> {}
@@ -20,7 +27,14 @@ fun addStudent (){
 fun updateStudentInfo(){
 
 }
-fun removeStudent(){
+fun removeStudent(id: Int){
+    val removedStudent = studentList.removeIf { it.id == id }
+
+    if (removedStudent) {
+        println("Student with ID $id removed successfully.")
+    } else {
+        println("No student found with ID $id.")
+    }
 
 }
 fun getStudents(){
